b71352603ecdeb0076d6dab0e5b98d1d33fca7e1
mailonvitordeoliveiraciril <mailon.ciril@escola.pr.gov.br>
2025-06-12 14:44:54 +0000

    exercicio 01 terminado

---
+++ exercicio01.py
@@ -0,0 +1,9 @@
+curso01 = int(input("digite a quatidade de acessos ao curso01"))
+curso02 = int(input("digite a quantidade de acessos ao curso02"))
+
+if curso01 > curso02:
+    print("o curso01 teve mais acessos.")
+elif curso02 > curso01:
+print("o curso02 teve mais acessos")
+else:
+    print("os cursos tiveram a mesma quantidade de acessos.")
