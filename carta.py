9accc4cb2147c76741782da98158c34b0dad31dd
mailonvitordeoliveiraciril <mailon.ciril@escola.pr.gov.br>
2025-06-17 12:10:54 +0000

    carta py pronto

---
+++ carta.py
@@ -0,0 +1,6 @@
+destinatario = input("digite o nome do destinatario: ")
+mensagem = input("digite sua mensagem: ")
+remetente = input ("digite seu nome: ")
+
+print("\n--- sua carta ---")
+print(f"ola {destinatario},\n\n{mensagem}\n\natenciosamente,\n{remetente}")
