bc67cdbee912242b41b2465a20e74cd6c59b94a8
mailonvitordeoliveiraciril <mailon.ciril@escola.pr.gov.br>
2025-06-26 11:51:11 +0000

    texto.py terminado

---
+++ texto.py
@@ -0,0 +1,11 @@
+#Exemplo basico
+print("ola mundo")
+
+#Concatenação com +
+nome = "mailon"
+sobrenome = "vitor"
+print("nome completo: " + nome + " " + sobrenome)
+
+#Concatenação com f-string
+idade = 16
+print(f"{nome} {sobrenome} tem {idade}")
